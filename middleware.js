b5eb2f280371bce5d1f2c6248cdfbd00639a8c42
piyush792 <57391086+piyush792@users.noreply.github.com>
2024-09-22 01:28:18 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 //create the middleware
 const reqFilter = (req, res, next) => {
     if (!req.query.age) {
-        res.send("Please provide age here2..")
+        res.send("Please Provide the AGE..")
     } else if (req.query.age <= 18) {
         res.send("You are not eligible to access this page!")
     } else {
